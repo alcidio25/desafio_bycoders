a8c502fce133825a9d69709c34ab3fabdc484a15
alcidio25 <66226980+alcidio25@users.noreply.github.com>
2021-05-28 18:55:08 +0100

    Add files via upload
    Interpretação e normalização dos dados do arquivo

--- app/controllers/cnab.js
+++ app/controllers/cnab.js
@@ -2,14 +2,36 @@
 * Criado por Alcidio Lucas - 28/05/2021
 */
 
+var lineReader = require('line-reader');
+
 module.exports.uploadArquivo = (aplicacao, req, res) => {
     if(req.files){
         if(this.validarArquivo(req.files.cnab, res)){
-            this.upload(req.files.cnab, res)
+            const cnab = this.upload(req.files.cnab, res)
+            this.converterCNAB(cnab);
         }
     }else{
         res.render('../views/index', {msg: 'Selecione um arquivo'});
     }
+}
+
+exports.converterCNAB = cnab => {
+    let dados = [];
+    lineReader.eachLine(cnab, function(line, last) {
+        linha = {};
+        linha.tipo = line.slice(0,1);
+        linha.data = line.slice(2,9);
+        linha.valor = line.slice(10,19);
+        linha.cpf = line.slice(20,30);
+        linha.cartao = line.slice(31,42);
+        linha.hora = line.slice(43,48);
+        linha.dono = line.slice(49,62);
+        linha.nome = line.slice(63,81);
+        dados.push(linha);
+        if (last) {
+            return false;
+        }
+    });
 }
 
 exports.upload = (cnab, res) => {
@@ -23,6 +45,7 @@ exports.upload = (cnab, res) => {
             res.render('../views/index', {msg: 'Arquivo carregado com sucesso'});
         }
     });
+    return nome_arquivo;
 }
 
 exports.validarArquivo = (arquivo, res) => {
